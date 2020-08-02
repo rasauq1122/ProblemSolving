2e9c0e3a4e0872f4d5c2914d139e4e619190ce97
rasauq1122 <rasauq1122@gmail.com>
2020-08-02 16:58:44 +0900

    Solve: BOJ11062 카드 게임 : 게임 이론 DP, 상대의 턴에는 최소를 저장하고, 나의 턴에는 최대를 저장한다.

---
+++ DP/BOJ11062.cpp
@@ -0,0 +1,77 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define endl '\n'
+#define blk ' '
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define ff first
+#define ss second
+#define int long long
+#define str string
+#define V(T) vector<T>
+#define pb push_back
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+#define Q(T) queue<T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define IMAX ((1LL<<31)-1)
+#define LMAX (((1LL<<62)-1)*2+1)
+#define MS(T) memset(T, -1, sizeof(T))
+#define ZS(T) memset(T, 0, sizeof(T))
+#define BIT(A,B) (!!((A)&(1LL<<(B))))
+#define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
+#define SHOW1(A,IX,EX) {cout<<'\n';for(int NI=(IX);NI<=(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define VV(T,N,M) V(V(T))(N,V(T)(M))
+#define ST(V) sort(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
+#define FIN() freopen("input.txt","r",stdin)
+#define FOUT() freopen("output.txt","w",stdout)
+#define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define BIG 1000000007
+using namespace std;
+
+int n, m, k, h=1, cnt, da, so = IMAX;
+int dp[1000][1000][2], arr[1000];
+
+int f(int n, int k, int t) {
+    if (n == k) {
+        if (t) return 0;
+        return arr[k];
+    }
+    if (dp[n][k][t] != -1) return dp[n][k][t];
+    if (t) {
+        int a = f(n+1,k,!t);
+        int b = f(n,k-1,!t);
+        if (a > b) return dp[n][k][t] = b;
+        return dp[n][k][t] = a;
+    }
+    int a = f(n+1,k,!t) + arr[n];
+    int b = f(n,k-1,!t) + arr[k];
+    if (a > b) return dp[n][k][t] = a;
+    return dp[n][k][t] = b;
+}
+
+signed main() {
+    IOSFAST();
+    TC() {
+        MS(dp);
+        cin >> n;
+        rep(i, 0, n) cin >> arr[i];
+        cout << f(0,n-1,0) << endl;
+    }
+}
