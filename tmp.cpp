51dd5a466eac0f5dd80dfc5f2b40c74976c7160b
rasauq1122 <rasauq1122@gmail.com>
2020-08-04 05:42:23 +0900

    Update: tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -26,20 +26,20 @@
 #define DQ(T) deque<T>
 #define IMAX ((1LL<<31)-1)
 #define LMAX (((1LL<<62)-1)*2+1)
-#define MS(T) memset(T, -1, sizeof(T))
-#define ZS(T) memset(T, 0, sizeof(T))
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
 #define BIT(A,B) (!!((A)&(1LL<<(B))))
 #define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
-#define SHOW1(A,IX,EX) {cout<<'\n';for(int NI=(IX);NI<=(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW1(A,IX,EX) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
 #define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
 #define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
-#define VV(T,N,M) V(V(T))(N,V(T)(M))
 #define ST(V) sort(V.begin(),V.end())
 #define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
 #define FIN() freopen("input.txt","r",stdin)
 #define FOUT() freopen("output.txt","w",stdout)
 #define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
 #define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
 #define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
 #define dd(a,b) !((a)%(b))
