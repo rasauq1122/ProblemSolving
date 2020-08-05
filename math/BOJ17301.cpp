371a6dec6e18918ae0c1162bd2a8f165ec166210
rasauq1122 <rasauq1122@gmail.com>
2020-08-05 16:47:47 +0900

    Solve: BOJ17301 NC 문자열 : N 문자열, C 문자열, CN 문자열을 찾은 후 가능한 모든 문자열에서 불가능한 문자열의 개수를 빼준다.

---
+++ math/BOJ17301.cpp
@@ -0,0 +1,97 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define endl '\n'
+#define blk ' '
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define ff first
+#define ss second
+#define int long long
+#define str string
+#define V(T) vector<T>
+#define pb push_back
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+#define Q(T) queue<T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define IMAX ((1LL<<31)-1)
+#define LMAX (((1LL<<62)-1)*2+1)
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
+#define BIT(A,B) (!!((A)&(1LL<<(B))))
+#define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
+#define SHOW1(A,IX,EX) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define ST(V) sort(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
+#define FIN() freopen("input.txt","r",stdin)
+#define FOUT() freopen("output.txt","w",stdout)
+#define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
+#define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define dd(a,b) !((a)%(b))
+#define sc1(x) int x;cin>>x;
+#define sc2(x,y) int x,y;cin>>x>>y;
+#define sc3(x,y,z) int x,y,z;cin>>x>>y>>z;
+#define BIG 1000000007
+using namespace std;
+
+int t, m, k, h=1, cnt, da, so = IMAX;
+int n, c, nc, cn, sum;
+
+signed main() {
+    IOSFAST();
+    cin >> t;
+    rep(i, 0, t) {
+        int nn = -1, cc = -1;
+        str s; cin >> s;
+        rep(i, 0, s.size()) {
+            if (s[i] == 'N' && nn == -1) nn = i;
+            if (s[i] == 'C') cc = i;
+        }
+        if (nn != -1) {
+            if (cc == -1) n++;
+            else {
+                if (nn < cc) nc++;
+                else cn++;
+            }
+        } else if (cc != -1) c++;
+    }
+
+    int k = 1, csum = 0, nsum = 0;
+    repc(i, 1, t) {
+        k = (t+1-i)*k%BIG;
+        sum += k;
+        sum %= BIG;
+    }
+
+    k = 1;
+    repc(i, 1, n) {
+        k = (n+1-i)*k%BIG;
+        nsum += k;
+        nsum %= BIG;
+    } 
+
+    k = 1;
+    repc(i, 1, c) {
+        k = (c+1-i)*k%BIG;
+        csum += k;
+        csum %= BIG;
+    } 
+
+    sum = (sum - (((csum+nsum+(csum*nsum)%BIG)%BIG)*(cn+1)+cn)%BIG + BIG)%BIG;
+    cout << sum;
+}
