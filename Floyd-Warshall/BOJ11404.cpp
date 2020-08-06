41e2039e7ab9b866cd2164db19a4efaccc79605c
rasauq1122 <rasauq1122@gmail.com>
2020-08-06 20:27:26 +0900

    Update: BOJ11404 플로이드 : 플로이드-와샬 알고리즘. 어떻게 3중 포문으로 모든 최단경로를 찾을 수 있는 것일까? 사실, 플로이드-와샬 알고리즘의 근본은 DP이다. f(i, j, k)는 i번째 정점에서 j번째 정점으로 1~k번의 정점만을 추가로 사용하여 만든 최단 경로의 비용이고 점화식은 min(f(i, j, k-1), f(i, k, k-1) + f(k, j, k-1))이다. f(i, k, x)는 x의 값이 어찌됐든 k번째 정점을 이미 포함하고 있기 때문에 k번째 정점을 아무런 비용없이 추가할 수 있는 것이다. 이는 결국 i - j 최단 경로를 추가로 사용하는 정점의 개수를 늘려나가면서 구할 수 있게 된다. 하지만, 보통 알고자하는 것은 f(i, j, n)의 값이므로 f(i, j, n-1)의 값을 기억할 필요가 없어 3중 for문으로도 구현을 할 수 있다.

--- Floyd-Warshall/BOJ11404.cpp
+++ Floyd-Warshall/BOJ11404.cpp
@@ -26,40 +26,66 @@
 #define DQ(T) deque<T>
 #define IMAX ((1LL<<31)-1)
 #define LMAX (((1LL<<62)-1)*2+1)
-#define MS(T) memset(T, -1, sizeof(T))
-#define ZS(T) memset(T, 0, sizeof(T))
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
 #define BIT(A,B) (!!((A)&(1LL<<(B))))
 #define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
-#define SHOW1(A,IX,EX) {cout<<'\n';for(int NI=IX;NI<=EX;NI++){cout<<A[NI]<<' ';}cout<<'\n';}
-#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=IX;NI<=EX;NI++){for(int NJ=IY;NJ<=EY;NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define SHOW1(A,IX,EX) {for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
 #define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
-#define VV(T,N,M) V(V(T))(N,V(T)(M))
 #define ST(V) sort(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
 #define FIN() freopen("input.txt","r",stdin)
 #define FOUT() freopen("output.txt","w",stdout)
 #define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
 #define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
-#define mod(A,B) ((A+B)%B)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define dd(a,b) !((a)%(b))
+#define sc(x) int x;cin>>x
+#define sc2(x,y) int x,y;cin>>x>>y
+#define sc3(x,y,z) int x,y,z;cin>>x>>y>>z
+#define BIG 1000000007
 using namespace std;
 
-int n, m, k;
+int n, m, k, h=1, cnt, da, so = IMAX;
+int dp[101][101][101];
+
+int foo(int i, int j, int k) { 
+    // i번 정점에서 j번 정점까지, 
+    // 1~k번의 정점을 추가로 사용했을 때의 최단 경로 
+    if (dp[k][i][j] != -1) return dp[k][i][j];
+    return dp[k][i][j] = min(foo(i, j, k-1), foo(i, k, k-1) + foo(k, j, k-1));
+    // i번 정점에서 j번 정점을 k번 정점을 추가로 사용하지 않을 때의 최단 경로의 비용.     
+    // 즉, 기존의 값
+    // i번 정점에서 k번 정점, k번 정점에서 j번 정점으로 이동했을 때의 최단 경로의 비용. 
+    // 즉, 새롭게 도출한 값
+    
+    // i - j 경로를 만드는 데 있어서, 사용하는 정점을 점점 늘리면서 결국 최단경로가 되게끔 유도하는 것
+    
+    // 결국 정점의 개수가 n일때만 의미 있으므로 dp로 괜히 모든 정보를 저장할 필요는 없고
+    // 3중 for문으로 해결할 수 있다.
+}
 
 signed main() {
     IOSFAST();
     cin >> n >> m;
-    V(V(int)) v(n,V(int)(n,IMAX)), d(n,V(int)(n,IMAX));
+    MS(dp);
+    repc(i, 1, n) repc(j, 1, n) dp[0][i][j] = IMAX;
+    repc(i, 1, n) dp[0][i][i] = 0;
+
     rep(i, 0, m) {
-        int x, y, c;
-        cin >> x >> y >> c;
-        v[x-1][y-1] = min(v[x-1][y-1], c);
+        sc3(x, y, z);
+        dp[0][x][y] = min(dp[0][x][y], z);
     }
-    rep(k, 0, n) rep(i, 0, n) rep(j, 0, n) {
-        if (i == j) continue;
-        v[i][j] = min(v[i][k] + v[k][j], v[i][j]);
-    }
-    rep(i, 0, n) {
-        rep(j, 0, n) cout << (v[i][j] == IMAX ? 0 : v[i][j]) << blk;
+
+    repc(i, 1, n) {
+        repc(j, 1, n) {
+            int k = foo(i, j, n);
+            cout << (k == IMAX ? 0 : k) << blk;  
+        }
         cout << endl;
     }
-}   
+
+}
