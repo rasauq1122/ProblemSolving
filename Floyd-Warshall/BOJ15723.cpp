18b5f61a33c335664760709b3b358633e140a11c
rasauq1122 <rasauq1122@gmail.com>
2020-08-07 15:51:35 +0900

    Solve: BOJ15723 n단 논법 : 기초적인 플로이드 와샬 문제

---
+++ Floyd-Warshall/BOJ15723.cpp
@@ -0,0 +1,73 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define endl '\n'
+#define blk ' '
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define ff first
+#define ss second
+#define int long long
+#define str string
+#define V(T) vector<T>
+#define pb push_back
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+#define Q(T) queue<T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define IMAX ((1LL<<31)-1)
+#define LMAX (((1LL<<62)-1)*2+1)
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
+#define BIT(A,B) (!!((A)&(1LL<<(B))))
+#define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
+#define SHOW1(A,IX,EX) {for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define ST(V) sort(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
+#define FIN() freopen("input.txt","r",stdin)
+#define FOUT() freopen("output.txt","w",stdout)
+#define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
+#define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define dd(a,b) !((a)%(b))
+#define sc(x) int x;cin>>x
+#define sc2(x,y) int x,y;cin>>x>>y
+#define sc3(x,y,z) int x,y,z;cin>>x>>y>>z
+#define BIG 1000000007
+using namespace std;
+
+int n, m, k, h=1, cnt, da, so = IMAX;
+int arr[26][26];
+
+signed main() {
+    IOSFAST();
+    cin >> n;
+    rep(i, 0, 26) rep(j, 0, 26) arr[i][j] = IMAX;
+    rep(i, 0, 26) arr[i][i] = 0;
+    rep(i, 0, n) {
+        char a, b;
+        string s;
+        cin >> a >> s >> b;
+        arr[a-'a'][b-'a'] = 0;
+    }
+    rep(k, 0, 26) rep(i, 0, 26) rep(j, 0, 26) arr[i][j] = min(arr[i][j], arr[i][k]+arr[k][j]);
+    TC() {
+        char a, b;
+        string s;
+        cin >> a >> s >> b;
+        cout << (arr[a-'a'][b-'a'] ? 'F' : 'T') << endl;
+    }
+}
