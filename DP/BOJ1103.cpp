41140bb0d13bf6575addfd4a8b94a436b758dd79
rasauq1122 <rasauq1122@gmail.com>
2020-08-07 20:52:35 +0900

    Solve: BOJ1103 게임 : DP를 DFS와 동시에 돌리면서 싸이클의 여부를 판단해주어야 한다.

---
+++ DP/BOJ1103.cpp
@@ -0,0 +1,84 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <math.h>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define endl '\n'
+#define blk ' '
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define ff first
+#define ss second
+#define int long long
+#define str string
+#define V(T) vector<T>
+#define pb push_back
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+#define Q(T) queue<T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define IMAX ((1LL<<31)-1)
+#define LMAX (((1LL<<62)-1)*2+1)
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
+#define BIT(A,B) (!!((A)&(1LL<<(B))))
+#define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
+#define SHOW1(A,IX,EX) {for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define ST(V) sort(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
+#define FIN() freopen("input.txt","r",stdin)
+#define FOUT() freopen("output.txt","w",stdout)
+#define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
+#define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define dd(a,b) !((a)%(b))
+#define sc(x) int x;cin>>x
+#define sc2(x,y) int x,y;cin>>x>>y
+#define sc3(x,y,z) int x,y,z;cin>>x>>y>>z
+#define scs(s) str s;cin>>s
+#define BIG 1000000007
+using namespace std;
+
+int n, m, k, h=1, cnt, da, so = IMAX;
+bool loop = false;
+int arr[50][50], vis[50][50], dp[50][50], dx[4] = {1,-1,0,0}, dy[4] = {0,0,1,-1};
+
+int dfs(int x, int y) {
+    if (x < 0 || y < 0 || x >= n || y >= m || arr[x][y] == -1) return 0;
+    if (loop) return n*m;
+    if (vis[x][y]) {
+        loop = true;
+        return n*m;
+    }
+    if (dp[x][y] != -1) return dp[x][y];
+    vis[x][y] = 1;
+    int da = 0;
+    rep(i, 0, 4) da = max(da, dfs(x+dx[i]*arr[x][y],y+dy[i]*arr[x][y]));
+    vis[x][y] = 0; 
+    return dp[x][y] = 1 + da;
+}
+
+signed main() {
+    IOSFAST();
+    cin >> n >> m;
+    rep(i, 0, n) {
+        scs(s);
+        rep(j, 0, m) arr[i][j] = (s[j] == 'H' ? -1 : s[j]-'0');
+    }
+    MS(dp);
+    k = dfs(0, 0);
+    if (k > n*m) cout << -1;
+    else cout << k;
+}
