73850c1322c462eba561bcd880757b760ce45192
rasauq1122 <rasauq1122@gmail.com>
2020-08-11 20:54:06 +0900

    Update: tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -33,9 +33,10 @@
 #define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
 #define SHOW1(A,IX,EX) {for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
 #define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
-#define IOSFAST() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define FASTIO() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
 #define ST(V) sort(V.begin(),V.end())
+#define RVS(V) reverse(V.begin(),V.end())
 #define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
 #define FIN() freopen("input.txt","r",stdin)
 #define FOUT() freopen("output.txt","w",stdout)
@@ -43,17 +44,23 @@
 #define repc(A,B,C) for(int A=(B);A<=(C);A++)
 #define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
 #define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
-#define dd(a,b) !((a)%(b))
+#define dd(a,b) (!((a)%(b)))
 #define sc(x) int x;cin>>x
 #define sc2(x,y) int x,y;cin>>x>>y
 #define sc3(x,y,z) int x,y,z;cin>>x>>y>>z
 #define scs(s) str s;cin>>s
+#define scc(c) char c;cin>>c
 #define BIG 1000000007
 using namespace std;
 
-int n, m, k, h=1, cnt, da, so = IMAX;
+int pow(int a, int b, int c) {if (!b) {return 1;} int rtn = pow(a, b/2, c); rtn *= rtn, rtn %= c; if (b%2) {rtn *= a, rtn %= c;} return rtn;}
+bool safe(int nx, int ny, int gx, int gy) {return 0 <= nx && nx < gx && 0 <= ny && ny < gy;}
+
+const int dx[8] = {1,-1,0,0,1,1,-1,-1}, dy[8] = {0,0,1,-1,1,-1,1,-1};
+
+int n, m, k, h=1, cnt, sum, da, so = IMAX;
 
 signed main() {
-    IOSFAST();
+    FASTIO();
     
 }
