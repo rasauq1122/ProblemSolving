42f165e515834ddbee5c7d50dfeaaacf46701329
rasauq1122 <rasauq1122@gmail.com>
2020-08-12 22:15:56 +0900

    Solve: BOJ2150 Strongly Connected Component : 기초적인 SCC 문제

---
+++ SCC/BOJ2150.cpp
@@ -0,0 +1,106 @@
+// 
+
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+#include <math.h>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#define endl '\n'
+#define blk ' '
+#define pii pair<int,int>
+#define piii pair<int,pii>
+#define ff first
+#define ss second
+#define int long long
+#define str string
+#define V(T) vector<T>
+#define pb push_back
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+#define Q(T) queue<T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define IMAX ((1LL<<31)-1)
+#define LMAX (((1LL<<62)-1)*2+1)
+#define MS(T) memset(T,-1,sizeof(T))
+#define ZS(T) memset(T,0,sizeof(T))
+#define BIT(A,B) (!!((A)&(1LL<<(B))))
+#define BITS(A,B,C) (((A)>>(B))&((1LL<<(C)-(B)+1)-1))
+#define SHOW1(A,IX,EX) {for(int NI=(IX);NI<(EX);NI++){cout<<A[NI]<<' ';}cout<<'\n';}
+#define SHOW2(A,IX,EX,IY,EY) {cout<<'\n';for(int NI=(IX);NI<(EX);NI++){for(int NJ=(IY);NJ<(EY);NJ++){cout<<A[NI][NJ]<<' ';}cout<<'\n';}}
+#define FASTIO() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define TC() int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
+#define ST(V) sort(V.begin(),V.end())
+#define RVS(V) reverse(V.begin(),V.end())
+#define PR(V) V.erase(unique(V.begin(),V.end()),V.end())
+#define FIN() freopen("input.txt","r",stdin)
+#define FOUT() freopen("output.txt","w",stdout)
+#define rep(A,B,C) for(int A=(B);A<(C);A++)
+#define repc(A,B,C) for(int A=(B);A<=(C);A++)
+#define per(A,B,C) for(int A=(C)-1;A>=(B);A--)
+#define lb(V,K) lower_bound(V.begin(),V.end(),K)-V.begin()
+#define dd(a,b) (!((a)%(b)))
+#define sc(x) int x;cin>>x
+#define sc2(x,y) int x,y;cin>>x>>y
+#define sc3(x,y,z) int x,y,z;cin>>x>>y>>z
+#define scs(s) str s;cin>>s
+#define scc(c) char c;cin>>c
+#define BIG 1000000007
+using namespace std;
+
+int pow(int a, int b, int c) {if (!b) {return 1;} int rtn = pow(a, b/2, c); rtn *= rtn, rtn %= c; if (b%2) {rtn *= a, rtn %= c;} return rtn;}
+bool safe(int nx, int ny, int gx, int gy) {return 0 <= nx && nx < gx && 0 <= ny && ny < gy;}
+
+const int dx[8] = {1,-1,0,0,1,1,-1,-1}, dy[8] = {0,0,1,-1,1,-1,1,-1};
+
+int n, m, k, h=1, cnt, sum, da, so = IMAX;
+int vis[10001], scc[10001], cnt2;
+V(V(int)) v(10001), ans(10001);
+S(int) s; 
+
+int dfs(int i) {
+    int so = vis[i] = ++cnt;
+    s.push(i);
+
+    rep(j, 0, v[i].size()) {
+        int nxt = v[i][j];
+        if (!vis[nxt]) so = min(so, dfs(nxt));
+        else if (!scc[nxt]) so = min(so, vis[nxt]);
+    }
+
+    if (so == vis[i]) {
+        cnt2++;
+        for(;!s.empty();s.pop()) {
+            scc[s.top()] = cnt2; 
+            if (s.top() == i) {
+                s.pop();
+                break;
+            }
+        }
+    }
+
+    return so;
+}
+
+signed main() {
+    FASTIO();
+    cin >> n >> m;
+    rep(i, 0, m) {
+        sc2(x,y);
+        v[x].pb(y);
+    }
+
+    repc(i, 1, n) if (!vis[i]) dfs(i);
+    repc(i, 1, n) ans[scc[i]].pb(i);
+    sort(ans.begin()+1, ans.begin()+cnt2+1);
+    cout << cnt2 << endl;
+    repc(i, 1, cnt2) {
+        rep(j, 0, ans[i].size()) cout << ans[i][j] << blk;
+        cout << "-1" << endl;
+    }
+}
