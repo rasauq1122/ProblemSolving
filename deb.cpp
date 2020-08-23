0ae02ca526aaa83233e513f3adbdbd9e1072c77f
rasauq1122 <rasauq1122@gmail.com>
2020-08-24 01:35:50 +0900

    Update: deb.cpp

--- deb.cpp
+++ deb.cpp
@@ -3,19 +3,12 @@
 #define prt(V)          {for(int NI=0;NI<(V).size();NI++){cout<<(V[NI])<<' ';}cout<<'\n';}
 #define prt2(V)         {for(int NI=0;NI<(V).size();NI++){for(int NJ=0;NJ<(V[NI]).size();NJ++){cout<<V[NI][NJ]<<' ';}cout<<'\n';}}
 
-#define pf(x)           cout<<(x)
-#define pfb(x)          cout<<(x)<<' '
-#define pf1(x)          cout<<(x)<<'\n'
-#define pf2(x,y)        cout<<(x)<<' '<<(y)<<'\n'
-#define pf3(x,y,z)      cout<<(x)<<' '<<(y)<<' '<<(z)<<'\n'
-#define pf4(x,y,z,w)    cout<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<'\n'
-#define pf5(x,y,z,w,u)  cout<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<' '<<(u)<<'\n'
-
-#define bpf1(x)         cout<<' '<<(x)<<'\n'
-#define bpf2(x,y)       cout<<' '<<(x)<<' '<<(y)<<'\n'
-#define bpf3(x,y,z)     cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<'\n'
-#define bpf4(x,y,z,w)   cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<'\n'
-#define bpf5(x,y,z,w,u) cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<' '<<(u)<<'\n'
+#define pf0()           cout<<'\n'
+#define pf1(x)          cout<<' '<<(x)<<'\n'
+#define pf2(x,y)        cout<<' '<<(x)<<' '<<(y)<<'\n'
+#define pf3(x,y,z)      cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<'\n'
+#define pf4(x,y,z,w)    cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<'\n'
+#define pf5(x,y,z,w,u)  cout<<' '<<(x)<<' '<<(y)<<' '<<(z)<<' '<<(w)<<' '<<(u)<<'\n'
 
 #define TC()            int TEST_CASE;cin>>TEST_CASE;for(int TEST_NUM=1;TEST_NUM<=TEST_CASE;TEST_NUM++)
 #define FASTIO()        ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
