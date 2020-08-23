f8abf0b0709a578abbcfc9cd24cc4a74f6a71f29
rasauq1122 <rasauq1122@gmail.com>
2020-08-23 20:51:06 +0900

    Update: deb.cpp

--- deb.cpp
+++ deb.cpp
@@ -3,12 +3,6 @@
 #define prt(V)          {for(int NI=0;NI<(V).size();NI++){cout<<(V[NI])<<' ';}cout<<'\n';}
 #define prt2(V)         {for(int NI=0;NI<(V).size();NI++){for(int NJ=0;NJ<(V[NI]).size();NJ++){cout<<V[NI][NJ]<<' ';}cout<<'\n';}}
 
-#define rd(x)           cin>>x
-#define rd2(x,y)        cin>>x>>y
-#define rd3(x,y,z)      cin>>x>>y>>z
-#define rd4(x,y,z,w)    cin>>x>>y>>z>>w
-#define rd5(x,y,z,w,u)  cin>>x>>y>>z>>w>>u
-
 #define pf(x)           cout<<(x)
 #define pfb(x)          cout<<(x)<<' '
 #define pf1(x)          cout<<(x)<<'\n'
@@ -31,9 +25,10 @@
 #define pii             pair<int,int>
 #define ff              first
 #define ss              second
-#define pb              push_back
 #define vi              vector<int>
+#define vp              vector<pair<int,int>>
 #define wi              vector<vector<int>>
+#define wp              vector<vector<pair<int,int>>>
 #define all(v)          (v).begin(), (v).end()
 #define INF             ((1LL<<31)-1)
 #define int             long long
